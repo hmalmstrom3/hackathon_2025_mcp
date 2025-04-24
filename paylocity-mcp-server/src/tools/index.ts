23e78e86813e04b8c0cd407272758f2ab3418576
SAnnavarpu <sannavarpu@paylocity.com>
2025-04-24 15:35:00 -0500

    Added List of Apis

--- paylocity-mcp-server/src/tools/index.ts
+++ paylocity-mcp-server/src/tools/index.ts
@@ -1,8 +1,25 @@
 import { McpToolDefinition } from "./definition.js";
 import { toolDefinitionMap as employeeDemographicToolDefinitionMap } from "./employee-demographics.js";
 import { toolDefinitionMap as paySetupToolDefinitionMap } from "./pay-setup.js";
+import { toolDefinitionMap as backgroundCheckDefinitionMap } from "./background-check.js";
+import { toolDefinitionMap as punchDetailDefinitionMap } from "./punch-detail-api.js";
+import { toolDefinitionMap as punchImportDefinitionMap } from "./punch-import-api.js";
+import { toolDefinitionMap as marketplacePayEntryDefinitionMap } from "./marketplace-pay-entry.js";
+import { toolDefinitionMap as payEntryDefinitionaMap } from "./pay-entry.js";
+import { toolDefinitionMap as schedulingDefinitionMap } from "./pay-entry.js";
+import { toolDefinitionMap as companyLevelInformationDefinitionMap } from "./company-level-information.js";
+
+
+
 
 export const toolDefinitionMap: Map<string, McpToolDefinition> = new Map([
     ...employeeDemographicToolDefinitionMap,
     ...paySetupToolDefinitionMap,
+    ...backgroundCheckDefinitionMap,
+    ...punchDetailDefinitionMap,
+    ...punchImportDefinitionMap,
+    ...marketplacePayEntryDefinitionMap,
+    ...payEntryDefinitionaMap,
+    ...schedulingDefinitionMap,
+    ...companyLevelInformationDefinitionMap
 ]);
